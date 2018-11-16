51bb955abede3dee7d9e5a9397fdf198dfbdec1e
heroes-coding <jeremy@heroes.report>
2018-11-16 13:41:13 -0600

    cors

--- index.js
+++ index.js
@@ -8,6 +8,20 @@ const path = require('path')
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080')
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST')
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', '*')
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  // Pass to next layer of middleware
+  next()
+})
+
 app.get('/', (req, res) => res.send('Hello World!'))
 app.post('/text',function(req,res){
   const text =req.body.text;
